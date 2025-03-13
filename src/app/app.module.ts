17dc17a8ec59ab06c8b9d8281cef91367edecc92
FloorG <floorgommers@gmail.com>
2025-03-13 12:28:27 +0100

    Added shopping cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ProductsComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+  ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatIconModule],
 })
 export class AppModule {}
