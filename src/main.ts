b4c40bad044fe7870b6cc3180b7b41cfe5a1aa02
FloorG <floorgommers@gmail.com>
2025-03-20 23:11:41 +0100

    dep fix

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { authInterceptor } from './app/interceptors/auth.interceptors';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(routes, withHashLocation()),
   ],
 }).catch((err) => console.error(err));
